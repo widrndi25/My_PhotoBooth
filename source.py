51e86e6a0938b44f0a53a246af6e221d564e5bd5
Lee JeongHyo <widrndi2@naver.com>
2023-12-05 17:37:02 +0900

    fix: code

--- source.py
+++ source.py
@@ -1,15 +1,4 @@
 import tkinter as tk
-def close_windows():
-    cap.release()
-    window.destroy()
-window = tk.Tk()
-window.title("나만의 포토 부스")
-window.geometry("880x680")
-window.protocol("WM_DELETE_WINDOW", close_windows)
-window.resizable(False, False)
-
-# GUI 시작
-window.mainloop()
 from PIL import Image, ImageTk, ImageGrab
 
 
@@ -22,7 +11,20 @@ def take_screenshot():
     screenshot = ImageGrab.grab(bbox=(x, y, x + width, y + height))
     screenshot.save("screenshot.png")
 
+
+def close_windows():
+    cap.release()
+    window.destroy()
+window = tk.Tk()
+window.title("나만의 포토 부스")
+window.geometry("880x680")
+window.protocol("WM_DELETE_WINDOW", close_windows)
+window.resizable(False, False)
+
 take_photo_button = tk.Button(window, text="Take a Photo", command=take_screenshot)
 take_photo_button.grid(row=4, column=1, pady=10)
+
+window.mainloop()
+
 
 
