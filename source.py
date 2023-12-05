ae0e5f0f95e3dbeafc4f6447ae8bf39e3c9c278e
naroroming <144348007+naroroming@users.noreply.github.com>
2023-12-05 17:34:23 +0900

    from PIL import Image, ImageTk, ImageGrab
    def take screenshot():
        x = window.winfo rootx()
        y = window.winfo rooty()
        width = window.winfo width()
        height = window.winfo height()
    
        screenshot = ImageGrab.grab(bbox=(x,y,x+ + width, y + height))
        screenshot.save("screenshot.png")
    
    take photo button = tk.Button(window, text="Take a Photo",command=take screenshot)
    take photo button.grid(row=4, column=1, pady=10)

--- source.py
+++ source.py
@@ -10,3 +10,19 @@ window.resizable(False, False)
 
 # GUI 시작
 window.mainloop()
+from PIL import Image, ImageTk, ImageGrab
+
+
+def take_screenshot():
+    x = window.winfo_rootx()
+    y = window.winfo_rooty()
+    width = window.winfo_width()
+    height = window.winfo_height()
+
+    screenshot = ImageGrab.grab(bbox=(x, y, x + width, y + height))
+    screenshot.save("screenshot.png")
+
+take_photo_button = tk.Button(window, text="Take a Photo", command=take_screenshot)
+take_photo_button.grid(row=4, column=1, pady=10)
+
+
